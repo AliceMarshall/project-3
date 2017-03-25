ab53c3d7c17d4a6ad575e0a9ae680c16efc14c3a
sarahlikeshiny <sarah.g.thomas@gmail.com>
2017-03-25 15:34:41 +0000

    SM added delete controller for modal

--- src/js/controllers/dateNights.js
+++ src/js/controllers/dateNights.js
@@ -3,7 +3,8 @@ angular
   .controller('dateNightsIndexCtrl', dateNightsIndexCtrl)
   .controller('dateNightsNewCtrl', dateNightsNewCtrl)
   .controller('dateNightsShowCtrl', dateNightsShowCtrl)
-  .controller('dateNightsEditCtrl', dateNightsEditCtrl);
+  .controller('dateNightsEditCtrl', dateNightsEditCtrl)
+  .controller('dateNightsDeleteCtrl', dateNightsDeleteCtrl);
 
 dateNightsIndexCtrl.$inject = ['DateNight'];
 function dateNightsIndexCtrl(DateNight) {
@@ -42,7 +43,7 @@ function dateNightsShowCtrl(DateNight, $stateParams, $state) {
   }
 
   vm.delete = dateNightsDelete;
-}  
+}
 
 dateNightsEditCtrl.$inject = ['DateNight', '$stateParams', '$state'];
 function dateNightsEditCtrl(DateNight, $stateParams, $state) {
@@ -57,4 +58,27 @@ function dateNightsEditCtrl(DateNight, $stateParams, $state) {
   }
 
   vm.update = dateNightsUpdate;
+}
+
+dateNightsDeleteCtrl.inject = ['$uibModalInstance', 'currentDateNight', '$state'];//instance of the modal thats just been opened. The currentBird is the item from resolve
+function dateNightsDeleteCtrl($uibModalInstance, currentDateNight, $state) {
+  const vm = this;
+  vm.dateNight = currentDateNight;
+
+  function closeModal() {
+    $uibModalInstance.close();
+  }
+  vm.close = closeModal;
+
+  function dateNightsDelete() {
+    vm.dateNight
+      .$remove()
+      .then(() => {
+        $state.go('dateNightsIndex');
+        $uibModalInstance.close();//go to birds index page and close modal
+      });
+  }
+
+  vm.delete = dateNightsDelete;
+
 }
