3103eacafc54f060af6fdac621ce928be03cf72c
AliceMarshall <marshall.alice92@gmail.com>
2017-03-29 17:21:36 +0100

    Fixing merge issues

--- src/js/controllers/dateNights.js
+++ src/js/controllers/dateNights.js
@@ -63,23 +63,24 @@ function DateNightsEditCtrl(DateNight, $stateParams, $state, $scope) {
   vm.update = dateNightsUpdate;
 
   function addFilm(film, showTime) {
-    // vm.dateNight.cinema = {};
+    console.log(film);
+    vm.dateNight.cinema = {};
     vm.dateNight.cinema.film = film;
     vm.dateNight.cinema.showTime = showTime;
   }
 
   vm.addFilm = addFilm;
 
-  function addCinema(lat, lng) {
-    console.log('lat and lng', lat, lng);
-    vm.dateNight.cinema = {};
+  // function addCinema(lat, lng) {
+  //   console.log('lat and lng', lat, lng);
+  //   vm.dateNight.cinema = {};
+  //
+  //   vm.dateNight.cinema.lat = lat;
+  //   vm.dateNight.cinema.lng = lng;
+  //   // $scope.apply;
+  // }
 
-    vm.dateNight.cinema.lat = lat;
-    vm.dateNight.cinema.lng = lng;
-    // $scope.apply;
-  }
-
-  $scope.addCinema = addCinema;
+  // $scope.addCinema = addCinema;
 
 }
 
