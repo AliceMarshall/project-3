732ecf20466301fc06ae084b197817daa3683fb9
AliceMarshall <marshall.alice92@gmail.com>
2017-03-30 11:29:49 +0100

    Restaurant is showing on restaurant page - Alice

--- src/js/controllers/restaurant.js
+++ src/js/controllers/restaurant.js
@@ -9,14 +9,19 @@ function RestaurantCtrl(restaurants, DateNight, $stateParams, $scope) {
   vm.date = {};
 
   DateNight.get($stateParams, (date) => {
-
     vm.date = date;
-    console.log('date', vm.date);
+    // console.log('date', vm.date);
   });
 
-  $scope.$watch(() => vm.date, getRestaurant);
+  $scope.$watch(() => vm.date, getRestaurants);
+  // $scope.$watch(() => vm.selected, getSelectedRestaurant);
+  //
+  // function getSelectedRestaurant() {
+  //
+  // }
 
-  function getRestaurant() {
+  function getRestaurants() {
+    if(!vm.date.cinema) return false;
     restaurants.getRestaurants(vm.date.cinema.lat, vm.date.cinema.lng)
     .then((data)=>{
       console.log('data', data);
